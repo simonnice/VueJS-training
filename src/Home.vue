35c22b2a86b590eb3b2c2298d5513daf38a9a533
sb22ix <sb22ix@student.lnu.se>
2019-06-12 15:56:06 +0200

    First commit

---
+++ src/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <app-server-status v-for="(server, i) in 5" :key="i"></app-server-status>
+  </div>
+</template>
+
+<script>
+import ServerStatus from "./ServerStatus.vue";
+export default {
+  components: {
+    "app-server-status": ServerStatus
+  }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import Home from "./Home.vue";
 
-Vue.config.productionTip = false
+Vue.component("app-servers", Home);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
